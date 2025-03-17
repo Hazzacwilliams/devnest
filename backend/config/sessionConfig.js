042226e8cfbc940c35057ace4c0a90b2b1ebce63
Harry Williams <hazzacwilliams@gmail.com>
2025-03-17 15:21:41 +0000

    updated to postgre-sql session store for production

--- backend/config/sessionConfig.js
+++ backend/config/sessionConfig.js
@@ -1,18 +1,21 @@
 import session from 'express-session';
-import dotenv from 'dotenv';
+import pgSession from 'connect-pg-simple';
+import pool from './dbConfig.js';
 
-dotenv.config();
+const PgSession = pgSession(session);
 
-//Set up express-session
 const sessionMiddleware = session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-        secure: process.env.NODE_ENV === ' production',
-        httpOnly: true,
-        maxAge: 1000 * 60 * 60 * 24
-    }
+  store: new PgSession({
+    pool, // your PostgreSQL connection pool
+    tableName: 'user_sessions', // table that will store sessions
+  }),
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    secure: process.env.NODE_ENV === 'production', // use HTTPS in production
+    maxAge: 1000 * 60 * 60 * 24, // 1 day, adjust if needed
+  }
 });
 
 export default sessionMiddleware;
