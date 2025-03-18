c4ab66dc57caf4345b839470f03706028cf10a83
Harry Williams <hazzacwilliams@gmail.com>
2025-03-18 23:29:30 +0000

    forgot to remove login route in userRoutes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import ensureAuthenticated from '../middleware/authMiddleware.js';
 import * as userController from '../controller/userController.js';
 import multer from 'multer';
 import jwtMiddleware from '../middleware/jwtMiddleware.js';
@@ -8,7 +7,6 @@ const upload = multer({ dest: 'uploads/' });
 const router = express.Router();
 
 //Public Routes
-router.post('/login', userController.loginUser);
 router.post('/', userController.createUser);
 
 
@@ -17,7 +15,6 @@ router.get('/', jwtMiddleware, userController.getAllUsers);
 router.get('/:userID', jwtMiddleware, userController.getUserById);
 router.put('/:userID', jwtMiddleware, userController.updateUser);
 router.delete('/:userID', jwtMiddleware, userController.deleteUser);
-router.get('/logout', jwtMiddleware, userController.logoutUser);
 router.post('/upload-profile-picture', upload.single('profilePicture'), userController.uploadProfilePicture);
 
 export default router;
