2f500a6df7dcb55f1682670f2a6a624794c2cb17
Harry Williams <hazzacwilliams@gmail.com>
2025-03-18 11:21:36 +0000

    changed dbconfig for use on render, hopefully fixes broken routing online

--- backend/config/dbConfig.js
+++ backend/config/dbConfig.js
@@ -5,12 +5,8 @@ dotenv.config();
 const { Pool } = pkg;
 
 const pool = new Pool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    ssl: process.env.DB_SSL === 'true'
+    connectionString: process.env.DATABASE_URL,
+    ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
 });
 
 export default pool;
